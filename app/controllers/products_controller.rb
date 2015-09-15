98188dc233ae29f48827e2090e8e2955f3323414
Oksana <lorelsblog@gmail.com>
2015-09-15 15:22:48 +0300

    add pagination

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,7 @@ class ProductsController < ApplicationController
   before_action :set_product, only: [:show]
   def index
     @products = Product.order("created_at desc")
+    .page(params[:page]).per(10)
   end
 
   def new
