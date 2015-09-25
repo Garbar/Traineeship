cec9f7407c7e6bdbf62c333a8bff204bf2f840cd
Oksana <lorelsblog@gmail.com>
2015-09-25 11:06:13 +0300

    add controller registrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'registrations', to: 'registrations#index', as: 'registration'
+  post 'registrations/:role', to: 'registrations#create', as: 'registrations'
   get 'profile', to: 'profile#index', as: 'profile'
   resources :products, only: [:index, :show, :new, :create] do
     post 'purchase', on: :member
